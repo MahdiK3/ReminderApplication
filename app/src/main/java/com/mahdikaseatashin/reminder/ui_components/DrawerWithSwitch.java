ccef79fd5a820c3fb78a7ef14ace671641a58f96
Mahdi <mahdikaseatashin@gmail.com>
2020-04-14 10:47:58 +0430

    + Create constructor plus getter setter

--- app/src/main/java/com/mahdikaseatashin/reminder/ui_components/DrawerWithSwitch.java
+++ app/src/main/java/com/mahdikaseatashin/reminder/ui_components/DrawerWithSwitch.java
@@ -1,5 +1,36 @@
 package com.mahdikaseatashin.reminder.ui_components;
 
 public class DrawerWithSwitch extends DrawerEntry {
+    private int icon_view;
+    private String icon_name;
+    private boolean checked;
 
+    public int getIcon_view() {
+        return icon_view;
+    }
+
+    public void setIcon_view(int icon_view) {
+        this.icon_view = icon_view;
+    }
+
+    public String getIcon_name() {
+        return icon_name;
+    }
+
+    public void setIcon_name(String icon_name) {
+        this.icon_name = icon_name;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public DrawerWithSwitch(int icon_view, String icon_name) {
+        this.icon_view = icon_view;
+        this.icon_name = icon_name;
+    }
 }
